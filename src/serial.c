35d474a43ca060fe38c2aecc44478b32ddb8370f
Kent Friesen <kfreezen@gmail.com>
2013-09-26 11:10:52 -0700

    Serial stubs added

---
+++ src/serial.c
@@ -0,0 +1,12 @@
+#include <serial.h>
+#include <dev.h>
+
+int serialPorts[4];
+
+int serialRead(void* buf, int len, VFS_Node* node) {
+
+}
+
+int serialWrite(const void* buf, int len, VFS_Node* node) {
+	
+}
